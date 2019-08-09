3d0c12e86d1743764f1383e9c6ec5648c27daf68
Joel Hilliard <jhilliard@Joels-MacBook-Pro.local>
2019-08-08 23:58:58 -0500

    Changed picture size

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,7 +10,7 @@ const IndexPage = () => {
     <div class="index-content">
 
       <div class="short-biography">
-        <img id = "user-image" src= {MainPhoto} />
+        <img id = "user-image" src= {MainPhoto} width = '125' height = '125' />
         <h1>I'm {Info.FullName}</h1>
         <h2>A {Info.StudentMajor} major at <a href={Info.SchoolWebsite}>{Info.SchoolName}</a></h2>
 
