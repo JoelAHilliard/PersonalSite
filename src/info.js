f1db120100f28bdfd479387ad9ef7e98880f01ee
Joel Hilliard <jhilliard@Joels-MacBook-Pro.local>
2019-08-08 22:24:58 -0500

    Fixed picture and updated contact info

--- src/info.js
+++ src/info.js
@@ -1,18 +1,19 @@
 import React from 'react';
 
-export const FullName = '' //Copy and paste your name between the quotes
+export const FullName = 'Joel Hilliard' //Copy and paste your name between the quotes
 
 export const GithubLink = '' // your Github link within the quotes
 
 export const LinkedinLink = '' //Copy and paste your Linkedin link within the quotes
 
-export const Email = 'mailto:#' //Replace # with email
+export const Email = 'mailto:#hilliardjoel05@gmail.com' //Replace # with email
 
-export const JobTitle = '' //Copy and paste your job title within the quotes
+export const JobTitle = 'Warehouse Worker' //Copy and paste your job title within the quotes
 
-export const CompanyName = '' //Copy and paste your company/school name within the quotes
+export const CompanyName = 'Origo'
+//Copy and paste your company/school name within the quotes
 
-export const CompanyWebsite = '' //Copy and paste your company/school name within the quotes
+export const CompanyWebsite = 'https://origoeducation.com' //Copy and paste your company/school name within the quotes
 
 //Replace /src/images/mainphoto.png with your own photo, and name it mainphoto.png
 
